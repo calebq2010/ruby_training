be8608381d85e785687f239e0221499721777b5a
Caleb Quinn <Caleb.Quinn@teamnorthwoods.com>
2018-06-25 12:25:56 -0400

    went over parellel variable assignment

---
+++ parallel_variable.rb
@@ -0,0 +1,20 @@
+# parallel variable assignment
+
+#a = 10
+#b = 20
+#c = 30
+
+# this is parallel variable assignents
+a, b, c = 10, 20, 30
+
+# you can print all on the same line like below
+p a, b, c
+
+# prints all on the same line
+print a,b,c
+
+# puts does the same at p
+puts a, b, c
+
+# this is assiging b = a and a = b
+a, b = b, a
