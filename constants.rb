ef9663f011a3300802172acc6bb5fcea64fcf624
Caleb Quinn <Caleb.Quinn@teamnorthwoods.com>
2018-06-25 12:51:26 -0400

    went over intro to object methods

---
+++ constants.rb
@@ -0,0 +1,13 @@
+age = 25
+p age
+
+age = 30
+p age
+
+#standard is to write a const variable is all caps
+NAME = "Caleb"
+PI = 3.14159
+
+p NAME, PI
+
+p "My name is #{NAME}"
---
+++ intro_to_object_methods.rb
@@ -0,0 +1,15 @@
+p "hello world".length
+p "hello world".upcase
+
+foo = "testing"
+
+p "the variable #{foo}"
+
+#incremints the number
+p 10.next
+
+puts
+
+puts "Hello world"
+#gives a more literal view on the value
+puts "Hello world".inspect
