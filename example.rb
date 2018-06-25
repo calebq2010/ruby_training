dfe0a5104d5d3697a4cd1d55a7684c5c25985b78
Caleb Quinn <Caleb.Quinn@teamnorthwoods.com>
2018-06-25 09:23:24 -0400

    finished puts method tut

--- example.rb
+++ example.rb
@@ -1,1 +1,14 @@
 puts "Hello World!"
+puts "I'm alive and well!"
+
+puts "I bought 5 dollars at the store."
+puts "I made $5 dollars or a profit of that"
+
+puts 5
+puts 3.14159
+
+puts 4 + 3
+puts "4" + "4"
+
+puts
+puts "testing line break with empty puts"
