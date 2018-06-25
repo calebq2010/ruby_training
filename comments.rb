44d9ff6f3fee77c5f185ddbe8f85ac2fe29b5ec9
Caleb Quinn <Caleb.Quinn@teamnorthwoods.com>
2018-06-25 10:06:57 -0400

    went over how to commit

---
+++ comments.rb
@@ -0,0 +1,18 @@
+# single line comment
+
+puts "Hello World" #prints hello world to console
+
+#multi line comment
+
+=begin
+testing the multi line comments
+to test out to see how it works
+and then testing it again
+=end
+
+# to comment multiple lines at once use ctrl + /
+# puts 1 + 1
+# puts 1 + 1
+# puts 1 + 1
+# puts 1 + 1
+# puts 1 + 1
