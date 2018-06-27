48faab0296646db5a3dfe9941e37797f640787c4
Caleb Quinn <calebq2010@gmail.com>
2018-06-26 20:18:03 -0400

    adding the puts method file

---
+++ the_puts_method.rb
@@ -0,0 +1,8 @@
+puts "Hello World"
+puts "I am alive and well!"
+
+puts "I bought $4.00 worth of beer"
+puts "I also am just testing the different characters that ruby can have"
+
+puts 5
+puts 4.134598387347839
